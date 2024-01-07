51ef2616be5128980039f93d1f5a9f6091876154
seanmcguinness3 <129340574+seanmcguinness3@users.noreply.github.com>
2024-01-07 11:47:31 -0800

    Update NonPolarFunctions.kt

--- app/src/main/java/com/example/bottomnavigationbarcomposeexample/NonPolarFunctions.kt
+++ app/src/main/java/com/example/bottomnavigationbarcomposeexample/NonPolarFunctions.kt
@@ -2,8 +2,11 @@ package com.example.bottomnavigationbarcomposeexample
 
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothGatt
 import android.bluetooth.BluetoothGattCallback
 import android.content.Context
+import android.os.Looper
+import java.util.logging.Handler
 
 @SuppressLint("MissingPermission")
 fun subscribeToVOMaster(device: BluetoothDevice, context: Context){
@@ -11,5 +14,16 @@ fun subscribeToVOMaster(device: BluetoothDevice, context: Context){
 }
 
 private val gattCallback = object :BluetoothGattCallback(){
+    @SuppressLint("MissingPermission")
+    override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
+        if (status == BluetoothGatt.GATT_SUCCESS){
+            if(newState == BluetoothGatt.STATE_CONNECTED){
+                android.os.Handler(Looper.getMainLooper()).post {
+                    gatt.discoverServices()
+                }
+            }
+        }
+    }
+
 
 }
